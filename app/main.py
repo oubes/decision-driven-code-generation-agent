26867c4eed9c6367f03d5201305d3b0a5371c5c9
oubes <omargamal.bs99@gmail.com>
2026-03-09 23:12:44 +0200

    removing comments

--- app/main.py
+++ app/main.py
@@ -8,18 +8,3 @@ app = FastAPI(
 )
 
 app.include_router(router)
-
-
-# test_questions = [
-#     "What is the total revenue?",
-#     "What is the average revenue per region?",
-#     "Revenue by product_category (top 3 categories)?",
-#     "Revenue per region per product_category?",
-#     "Show all sales rows.",
-# ]
-
-# for q in test_questions:
-#     print(f"\nQuestion: {q}")
-#     result = run_agent(q)
-#     print("Answer:", result)
-#     print("-"*60)
